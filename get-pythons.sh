086bd561ee2e48b37d59f9a54f66230bcb1f2621
Barry Warsaw <barry@python.org>
2019-02-05 09:55:05 -0800

    Debugging

--- get-pythons.sh
+++ get-pythons.sh
@@ -22,11 +22,11 @@ get_install () {
 
 
 # Install Python 2.7, 3.4, 3.5, 3.6, 3.7, 3.8
-get_install $PYTHON_27_VER
-get_install $PYTHON_34_VER
-get_install $PYTHON_35_VER
-get_install $PYTHON_36_VER
-get_install $PYTHON_37_VER
+#get_install $PYTHON_27_VER
+#get_install $PYTHON_34_VER
+#get_install $PYTHON_35_VER
+#get_install $PYTHON_36_VER
+#get_install $PYTHON_37_VER
 get_install $PYTHON_38_VER
 
 # Get and install Python rolling devel from the latest git install.
