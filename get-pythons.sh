0b841aab756ce94d6781ead160f696ae77d3d443
Barry Warsaw <barry@python.org>
2019-02-05 13:38:42 -0800

    Fix 3.8.0 directory

--- get-pythons.sh
+++ get-pythons.sh
@@ -22,21 +22,21 @@ get_install () {
 
 
 # Install Python 2.7, 3.4, 3.5, 3.6, 3.7, 3.8
-#get_install $PYTHON_27_VER
-#get_install $PYTHON_34_VER
-#get_install $PYTHON_35_VER
-#get_install $PYTHON_36_VER
-#get_install $PYTHON_37_VER
-#get_install $PYTHON_38_VER
+get_install $PYTHON_27_VER
+get_install $PYTHON_34_VER
+get_install $PYTHON_35_VER
+get_install $PYTHON_36_VER
+get_install $PYTHON_37_VER
+get_install $PYTHON_38_VER 3.8.0
 
 # Get and install Python rolling devel from the latest git install.
-#cd  /tmp/
-#wget -q https://github.com/python/cpython/archive/master.zip
-#unzip -qq master.zip
-#cd /tmp/cpython-master
-#./configure && make && make altinstall
+cd  /tmp/
+wget -q https://github.com/python/cpython/archive/master.zip
+unzip -qq master.zip
+cd /tmp/cpython-master
+./configure && make && make altinstall
 # Remove the git clone.
-#rm -r /tmp/cpython-master && rm /tmp/master.zip
+rm -r /tmp/cpython-master && rm /tmp/master.zip
 
 # After we have installed all the things, we cleanup tests and unused files
 # like .pyc and .pyo
