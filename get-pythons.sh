b6d3a7dd352469fccced6535d5d74983e051b608
Barry Warsaw <barry@python.org>
2019-02-04 17:18:00 -0800

    Skip master

--- get-pythons.sh
+++ get-pythons.sh
@@ -21,15 +21,6 @@ get_install () {
 }
 
 
-# First, get and install Python rolling devel from the latest git install.
-cd  /tmp/
-wget -q https://github.com/python/cpython/archive/master.zip
-unzip -qq master.zip
-cd /tmp/cpython-master
-./configure && make && make altinstall
-# Remove the git clone.
-rm -r /tmp/cpython-master && rm /tmp/master.zip
-
 # Install Python 2.7, 3.4, 3.5, 3.6, 3.7, 3.8
 get_install $PYTHON_27_VER
 get_install $PYTHON_34_VER
@@ -37,6 +28,15 @@ get_install $PYTHON_35_VER
 get_install $PYTHON_36_VER
 get_install $PYTHON_37_VER
 get_install $PYTHON_38_VER
+
+# Get and install Python rolling devel from the latest git install.
+#cd  /tmp/
+#wget -q https://github.com/python/cpython/archive/master.zip
+#unzip -qq master.zip
+#cd /tmp/cpython-master
+#./configure && make && make altinstall
+# Remove the git clone.
+#rm -r /tmp/cpython-master && rm /tmp/master.zip
 
 # After we have installed all the things, we cleanup tests and unused files
 # like .pyc and .pyo
